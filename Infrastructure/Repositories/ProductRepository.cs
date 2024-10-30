ec2805a6f0a041254a957ba7c30da8e58af7c91d
Evelina <krugerevelina@gmail.com>
2024-10-30 09:58:57 +0100

    started

---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Infrastructure.Dtos;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.Net.Http.Json;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly HttpClient _httpClient;
+        public ProductRepository(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(configuration["ApiSettings:ProductsApiUrl"] 
+                ?? throw new InvalidOperationException("Products API URL is missing in the configuration."));
+        }
+
+        public async Task<IEnumerable<CategoryDto>> GetCategoriesAsync()
+        {
+            try
+            {
+                string request = $"api/categories";
+                var result = await _httpClient.GetFromJsonAsync<IEnumerable<CategoryDto>>(request);
+                return result!;
+            }
+            catch (Exception ex) { Debug.WriteLine(ex); }
+            return null!;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProductsAsync(int categoryId)
+        {
+            try
+            {
+                string request = $"api/products?categoryId={categoryId}";
+                var result = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>(request);
+                return result!;
+            }
+            catch (Exception ex) { Debug.WriteLine(ex); }
+            return null!;
+        }
+        public async Task<ProductDto> GetProductAsync(int productId)
+        {
+            try
+            {
+                string requestUri = $"api/products/{productId}";
+                var result = await _httpClient.GetFromJsonAsync<ProductDto>(requestUri);
+                return result!;
+            }
+            catch (Exception ex) { Debug.WriteLine(ex); }
+            return null!;
+        }
+    }
+}
---
+++ Infrastructure/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Dtos;
+using Infrastructure.Repositories;
+
+namespace Infrastructure.Services
+{
+    public class ProductService(ProductRepository productRepository)
+    {
+        private readonly ProductRepository _productRepository = productRepository;
+
+        // gets all categories
+        public async Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync()
+        {
+            return await _productRepository.GetCategoriesAsync();
+        }
+
+        // gets all products from a specific category by Id
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync(int categoryId)
+        {
+            return await _productRepository.GetProductsAsync(categoryId);
+        }
+
+        // gets one product
+        public async Task<ProductDto> GetOneProductAsync(int productId)
+        {
+            return await _productRepository.GetProductAsync(productId);
+        }
+
+        // get one catgory?? can be added, but currently not needed
+    }
+}
