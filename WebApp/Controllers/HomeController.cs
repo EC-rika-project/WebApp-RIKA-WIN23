4809a2db09e837ef829e6466ed33091e4bee4cde
Andreas <pingis00@hotmail.com>
2024-10-31 11:13:03 +0100

    Added toast-config

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace WebApp.Controllers
         public IActionResult Index()
         {
             HomeIndexViewModel viewModel = new();
+            TempData["Message"] = "Detta är ett test-meddelande!";
+            TempData["MessageType"] = "warning";
             return View(viewModel);            
                 
         }
