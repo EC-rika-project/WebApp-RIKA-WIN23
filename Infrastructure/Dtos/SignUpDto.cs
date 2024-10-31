fcbf56be62cb200ccd9774e1ca5f87ac7b6842f9
Alexander <alexander.dyplin@utb.ecutbildning.se>
2024-10-31 10:57:26 +0100

    Changed the data handled in signupDto

--- Infrastructure/Dtos/SignUpDto.cs
+++ Infrastructure/Dtos/SignUpDto.cs
@@ -5,6 +5,5 @@ public class SignUpDto
     public string UserName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
-    public string ConfirmPassword { get; set; } = null!;
-    public bool TermsAndConditions { get; set; }
+
 }
