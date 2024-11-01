0801456dba30a1745e4fbba3cf8186ac350497b0
Andreas Fredriksson <afredriksson89@gmail.com>
2024-11-01 12:36:42 +0100

    In progress cart setup

---
+++ Infrastructure/Dtos/CartItemDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Dtos;
+
+public class CartItemDto
+{
+    public string ArticleNumber { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string Ingress { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal GetTotalPrice()
+    {
+        return Price * Quantity;
+    }
+
+}
---
+++ WebApp/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly ILogger<OrderController> _logger;
+
+    public OrderController(ILogger<OrderController> logger)
+    {
+        _logger = logger;
+    }
+
+    public List<CartItemDto> cartItems = new List<CartItemDto>
+    {
+        new CartItemDto { ArticleNumber = "1", Name = "Produkt 1", Price = 100, Ingress = "ingress", Quantity = 2 },
+        new CartItemDto { ArticleNumber = "2", Name = "Produkt 2", Price = 200, Ingress = "ingress", Quantity = 1 },
+    };
+
+    [Route("/orders")]
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+
+    public async Task<IActionResult> Cart()
+    {
+        CartViewModel viewModel = new();
+        decimal totalAmount = cartItems.Sum(x => x.GetTotalPrice());
+        ViewBag.TotalAmount = totalAmount;
+        //return View(viewModel);
+        return PartialView("Partials/Cart", viewModel);
+    }
+}
