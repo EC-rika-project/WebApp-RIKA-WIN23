921bb0ec72cdf256df402429bc3f5e80f3d2f0c9
Alexander <alexander.dyplin@utb.ecutbildning.se>
2024-11-01 12:04:40 +0100

    Added firstname and lastname to DTO and viewmodel. Added API connection

--- WebApp/Controllers/SignUpController.cs
+++ WebApp/Controllers/SignUpController.cs
@@ -1,19 +1,17 @@
 ﻿using Infrastructure.Dtos;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 using WebApp.ViewModels;
 namespace WebApp.Controllers;
 
+
 public class SignUpController : Controller
 {
-    private readonly UserManager<IdentityUser> _userManager;
     private readonly IHttpClientFactory _httpClientFactory;
 
-    public SignUpController(UserManager<IdentityUser> userManager, IHttpClientFactory httpClientFactory)
+    public SignUpController(IHttpClientFactory httpClientFactory)
     {
-        _userManager = userManager;
         _httpClientFactory = httpClientFactory;
     }
 
@@ -31,55 +29,33 @@ public class SignUpController : Controller
     {
         if (!ModelState.IsValid)
         {
-            TempData["Error"] = "Var vänlig och korrigera de markerade felen.";
             return View(viewModel);
         }
 
-        var exists = await _userManager.FindByEmailAsync(viewModel.Email);
-        if (exists != null)
+        var dto = new SignUpDto
         {
-            TempData["Error"] = "Användare med denna e-postadress finns redan.";
-            return View(viewModel);
-        }
-
-        var user = new IdentityUser
-        {
-            UserName = viewModel.UserName,
-            Email = viewModel.Email
+            FirstName = viewModel.FirstName,
+            LastName = viewModel.LastName,
+            Email = viewModel.Email,
+            Password = viewModel.Password
         };
 
+        var jsonContent = new StringContent(JsonConvert.SerializeObject(dto), Encoding.UTF8, "application/json");
+        var client = _httpClientFactory.CreateClient();
 
-        var result = await _userManager.CreateAsync(user, viewModel.Password);
-        if (result.Succeeded)
+        var response = await client.PostAsync("https://localhost:7163/api/signup", jsonContent);
+
+
+        if (!response.IsSuccessStatusCode)
         {
-
-            var dto = new SignUpDto
-            {
-                UserName = viewModel.UserName,
-                Email = viewModel.Email,
-                Password = viewModel.Password
-            };
-
-
-            var jsonContent = new StringContent(JsonConvert.SerializeObject(dto), Encoding.UTF8, "application/json");
-            var client = _httpClientFactory.CreateClient();
-            var response = await client.PostAsync("", jsonContent); //lägg till api länk
-
-            if (response.IsSuccessStatusCode)
-            {
-                TempData["Success"] = "Användaren har skapats framgångsrikt.";
-                return RedirectToAction("SignIn", "SignIn");
-            }
-
-
-            var errorContent = await response.Content.ReadAsStringAsync();
-            TempData["Error"] = $"Kunde inte skicka data till API:t: {errorContent}";
+            TempData["ErrorMessage"] = "Användare med denna e-postadress finns redan.";
             return View(viewModel);
         }
-            foreach (var error in result.Errors)
-            {
-                TempData["Error"] = error.Description; 
-            }
+
+        TempData["SuccessMessage"] = "Registreringen lyckades! Vänligen logga in.";
         return View(viewModel);
+        //RedirectToAction("SignIn", "SignIn");
     }
 }
+
+
--- WebApp/ViewModels/SignUpViewModel.cs
+++ WebApp/ViewModels/SignUpViewModel.cs
@@ -6,12 +6,18 @@ namespace WebApp.ViewModels;
 public class SignUpViewModel
 {
 	[DataType(DataType.Text)]
-	[Display(Name = "Username", Prompt = "Enter your username", Order = 0)]
-	[Required (ErrorMessage = "A valid Username is required")]
-	[MinLength(2, ErrorMessage = "Enter a valid username")]
-	public string UserName { get; set; } = null!;
+	[Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+	[Required (ErrorMessage = "A valid first name is required")]
+	[MinLength(2, ErrorMessage = "Enter a valid first name")]
+	public string FirstName { get; set; } = null!;
 
-	[Display(Name = "Email", Prompt = "Enter your Email address", Order = 1)]
+    [DataType(DataType.Text)]
+    [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 0)]
+    [Required(ErrorMessage = "A valid Username is required")]
+    [MinLength(2, ErrorMessage = "Enter a valid last name")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Email", Prompt = "Enter your Email address", Order = 1)]
 	[DataType(DataType.EmailAddress)]
 	[Required(ErrorMessage = "A valid Email is required")]
 	[RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Please enter a valid e-mail adress")]
