5895e938d023b10bc39187c9e2b720461685c82a
codingbyahlv <code@ahlv.se>
2024-11-04 11:23:28 +0100

    cart added

--- WebApp/Controllers/OrderController.cs
+++ WebApp/Controllers/OrderController.cs
@@ -13,25 +13,9 @@ public class OrderController : Controller
         _logger = logger;
     }
 
-    public List<CartItemDto> cartItems = new List<CartItemDto>
+    [Route("/checkout")]
+    public IActionResult Checkout()
     {
-        new CartItemDto { ArticleNumber = "1", Name = "Produkt 1", Price = 100, Ingress = "ingress", Quantity = 2 },
-        new CartItemDto { ArticleNumber = "2", Name = "Produkt 2", Price = 200, Ingress = "ingress", Quantity = 1 },
-    };
-
-    [Route("/orders")]
-    public IActionResult Index()
-    {
-
         return View();
-    }
-
-    public async Task<IActionResult> Cart()
-    {
-        CartViewModel viewModel = new();
-        decimal totalAmount = cartItems.Sum(x => x.GetTotalPrice());
-        ViewBag.TotalAmount = totalAmount;
-        //return View(viewModel);
-        return PartialView("Partials/Cart", viewModel);
     }
 }
---
+++ WebApp/wwwroot/js/cart.js
@@ -0,0 +1,32 @@
+﻿const openCartBtns = document.querySelectorAll(".cart-btn-open");
+const closeCartBtns = document.querySelectorAll(".cart-btn-close");
+const cart = document.querySelector(".cart");
+
+
+function cartToggle() {
+    if (cart.classList.contains("hide")) {
+        cart.classList.remove("hide");
+        cart.classList.add("show");
+        //fetchCart();
+    } else {
+        cart.classList.remove("show");
+        cart.classList.add("hide");
+    }
+}
+
+window.addEventListener("load", function () {
+    openCartBtns.forEach(btn => btn.addEventListener("click", cartToggle));
+    closeCartBtns.forEach(btn => btn.addEventListener("click", cartToggle));
+});
+
+function fetchCart() {
+    console.log("fetchCart")
+    //fetch("/Cart/Index")
+    //    .then(response => response.text())
+    //    .then(html => {
+    //        const overlayContainer = document.getElementById("cartOverlayContainer");
+    //        overlayContainer.innerHTML = html;
+    //        overlayContainer.style.display = "block";
+    //    })
+    //    .catch(error => console.error("Error loading cart items:", error));
+}
